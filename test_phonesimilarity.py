1ed66362fce7013677b9d95b72c783ffe2af7646
Arif Khan <arif.khan@dfki.de>
2017-03-07 12:36:42 +0100

    some chages

--- test_phonesimilarity.py
+++ test_phonesimilarity.py
@@ -1,11 +1,9 @@
 #!/bin/bash/python3
 
-from ps import phone, similarity;
+from phonesimilarity import phone, similarity;
 p1 = phone('AE')
 p2 = phone('SIL')
 
 s = similarity()
 sim = s.cosinesimilarity('AE', 'SIL')
-print(sim)
-sim = s.normsimilarity('SIL','SIL')
 print(sim)
