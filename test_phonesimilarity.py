4a0909ad1a7c5be32472fe727ecfb20a4fd8591a
Arif Khan <arif.khan@dfki.de>
2017-04-03 10:53:51 +0200

    change from sim to dist

--- test_phonesimilarity.py
+++ test_phonesimilarity.py
@@ -6,9 +6,8 @@ import os
 
 from phonesimilarity import phone, distance;
 
-sim = distance().jaccardDistance
-#sim = distance().simpleMatchingDistance
-
+dis = distance().jaccardDistance
+#dis = distance().simpleMatchingDistance
 
 ftable = open('spe_features_zeros.csv', 'r')
 #ftable = open('sampel_feat.csv', 'r')
@@ -22,8 +21,8 @@ cmatrix = [[0 for x in range(w)] for y in range(h)]
 
 for i, f in enumerate(phones_list):
     for j, g in enumerate(phones_list):
-        cmatrix[i][j] = sim(f,g)
-        #print("smd of {} and {} is {}".format(f, g, sim(f,g)))
+        cmatrix[i][j] = dis(f,g)
+        #print("smd of {} and {} is {}".format(f, g, dis(f,g)))
 
 mat = np.array(cmatrix)
 mini = np.min(mat, axis=0)
@@ -45,5 +44,5 @@ ax.set_xticks(np.arange(len(cmatrix)) + 0.5, minor=False)
 ax.set_yticks(np.arange(len(cmatrix)) + 0.5, minor=False)
 ax.set_xticklabels(list(phones_list), minor=False, fontsize=4)
 ax.set_yticklabels(list(phones_list), minor=False, fontsize=4)
-filename = os.path.join(figuredir, ftable.name.split(".")[0] + '_' +sim.__name__ + '.pdf')
+filename = os.path.join(figuredir, ftable.name.split(".")[0] + '_' +dis.__name__ + '.pdf')
 plt.savefig(filename, format='pdf')
